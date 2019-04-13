052b6f67b7b672c8d6a665b8ce2d2fc9554168ec
Aquib Baig <aquibbaig97@gmail.com>
2019-04-13 14:03:03 +0530

    homepage for the website

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <h1>About gatsby</h1>
+  </div>
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react";
+import Navbar from "../components/navbar";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
-export default () => <div>Hello world!</div>;
+export default () => (
+  <div>
+    <Navbar />
+  </div>
+);
