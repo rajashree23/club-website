075a0eec9abe91135af92c7f1bd78d787a507ae2
Aquib Baig <aquibbaig97@gmail.com>
2019-04-13 16:27:28 +0530

    frontpage image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import Navbar from "../components/navbar";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import Sitecontent from "../components/sitecontent";
 
 export default () => (
   <div>
     <Navbar />
+    <Sitecontent />
   </div>
 );
