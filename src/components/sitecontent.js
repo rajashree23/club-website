3ab8af2f4286a5a7d38c38b94506ae1bcd37826d
Aquib Baig <aquibbaig97@gmail.com>
2019-04-13 19:44:01 +0530

    rewrite readme

--- src/components/sitecontent.js
+++ src/components/sitecontent.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 export default () => (
-  <div className="container-fluid">
-    <div className="row">
-      <div className="col-md-10 col-lg-10 col-sm-12 col-xs-12">
-        {/* TODO:image here */}
-      </div>
-    </div>
+  <div className="container">
+    <div className="row" />
   </div>
 );
