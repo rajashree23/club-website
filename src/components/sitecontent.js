3eb1f40cdec3cb857cdd1d20ceef7fff01149648
Aquib Baig <aquibbaig97@gmail.com>
2019-04-13 17:02:55 +0530

    TODO:homepage

--- src/components/sitecontent.js
+++ src/components/sitecontent.js
@@ -3,14 +3,9 @@ import React from "react";
 export default () => (
   <div className="container-fluid">
     <div className="row">
-      <div className="col-md-8 col-lg-8 col-sm-12 col-xs-12">
-        <img
-          src="/images/index.jpg"
-          alt="homepage image"
-          style={{ width: `100%` }}
-        />
+      <div className="col-md-10 col-lg-10 col-sm-12 col-xs-12">
+        {/* TODO:image here */}
       </div>
-      <div className="col-md-4 col-lg-4 col-sm-12 col-xs-12" />
     </div>
   </div>
 );
