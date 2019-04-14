803965f368373245d5e30247b655dceba408a65e
Aquib Baig <aquibbaig97@gmail.com>
2019-04-14 11:31:51 +0530

    remove bootstrap 4 completely

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Navbar from "../components/navbar";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
 
 export default () => (
   <div>
