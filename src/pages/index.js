bc76cae50a1771bb8cd49c73e7298a6e2b4f7482
Aquib Baig <aquibbaig97@gmail.com>
2019-04-16 01:19:14 +0530

    typography element

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Navigationbar from "../components/navbar";
+import Typography from "@material-ui/core/Typography";
 
 const styles = {
   width: `100%`
+};
+
+const fontFamily = {
+  fontFamily: `Courier New`
 };
 
 class Index extends React.Component {
@@ -11,6 +16,15 @@ class Index extends React.Component {
       <div>
         <Navigationbar />
         <img src="index.jpg" style={styles} />
+        <Typography
+          component="h2"
+          align="center"
+          variant="headline"
+          gutterBottom
+          style={fontFamily}
+        >
+          We are Zairza
+        </Typography>
       </div>
     );
   }
